7a0a02fdaea23f76aad0e7d669c309ac4c0473fa
John Pieri <jvpieri@gmail.com>
2023-06-03 15:01:55 -0400

    Commit

--- Characters.js
+++ Characters.js
@@ -1,4 +1,4 @@
-import {getDiceRollArray} from './utils.js'
+import {getDiceRollArray, getDicePlaceholderHtml} from './utils.js'
 
 function Characters(data) {
     Object.assign(this, data)
--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import charactersData from './data.js'
 import Characters from './Characters.js'
 
+document.getElementById('attack-button').addEventListener('click', attack)
+
+function attack() {
+    console.log("click")
+    render()
+}
+
 function render() {
     document.getElementById('hero').innerHTML = wizard.getCharacter()
     document.getElementById('monster').innerHTML = orc.getCharacter()
